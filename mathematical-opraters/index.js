8b88d6b60bd593413ee11bebe75b8bd64809141e
abhishek tyagi <abhishektyagi@192.168.1.3>
2022-12-31 21:33:46 +0530

    day 2

---
+++ mathematical-opraters/index.js
@@ -0,0 +1,17 @@
+// let a=3;
+// let b=2;
+// console.log("sum is", a+b);
+// console.log("sub is", a-b);
+// console.log("multiply is" ,a*b);
+// console.log("div", a/b);
+// console.log("mod, a%b)
+
+// let a=10;
+// let b=4;
+// let c=5;
+
+// console.log((a+b)+(a*c)+(a/c)+(a-c)+(b**c)+(c-b)+(b%c));
+
+let x=25;
+
+console.log(x**(1/2));
